50d452bd503a8b1ebb3d4d8a4e70022920f6c722
Thinh Le <54289320+heydayle@users.noreply.github.com>
2026-01-07 05:41:29 +0000

    chore: font family mono

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -10,7 +10,7 @@ export function Home() {
     const { user } = useUser();
 
     return (
-        <div className='p-6 max-w-[1200px] mx-auto relative'>
+        <div className='p-6 max-w-[1200px] mx-auto relative font-mono'>
             <div className="sticky top-0 z-10 flex justify-between p-4 bg-black/20 filter backdrop-blur-sm">
                 <h1 className='text-center text-2xl font-bold'>{t("numberology")}</h1>
                 {step === STEPS.Result && 
